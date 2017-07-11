4aa11079c0eef7dcddda5babe976513a65ab2553
Mattia <Mattia@10.0.92.251>
2017-07-11 20:32:48 +0200

    Aggiornata la pagina di registrazione Aggiiunta la navbar nel login

--- web/js/Mat.js
+++ web/js/Mat.js
@@ -1,17 +1,73 @@
-function selezionaCategoria(filtro){
-    var $filter=filtro.trim();
+
+function confrontaPassword()
+{
+    var send = true;
+    if (($("#username").val() == null) || ($("#username").val() == "") || ($("#username").val() == "null"))
+    {
+        console.log("userVuoto");
+        send = false;
+    }
+    if (($("#password").val() == null) || ($("#password").val() == "") || ($("#password").val() == "null"))
+    {
+        console.log("pwdVuota");
+        send = false;
+    }
+    if (($("#password_confirm").val() == null) || ($("#password_confirm").val() == "") || ($("#password_confirm").val() == "null"))
+    {
+        console.log("pwdCvuota");
+        send = false;
+    }
+    if(send == true)
+    {
+        if($("#form-user").hasClass("has-error"))
+        {
+           console.log("username");
+            send = false;
+        }
+    }
+     if(send == true)
+    {
+        if($("#password").val() !== $("#password_confirm").val())
+        {
+            console.log("pwdDiverse");
+            send = false;
+        }
+    }
+    
+    if (send == true)
+    {
+        $("#daAbilitare").removeClass("disabled");
+    }
+    else
+    {
+        $("#daAbilitare").addClass("disabled");
+    }
+}
+
+
+$('#centra').on('keyup keypress', function(e) {
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === 13) { 
+    e.preventDefault();
+    return false;
+  }
+});
+
+function selezionaCategoria(filtro) {
+    var $filter = filtro.trim();
     var $ciao;
-    $(".categorie").each(function(){
-        $ciao= $(this).text().trim();
-        if( $ciao === $filter){
-            
-            $(this).css("background","#286090");
-            $(this).css("color","white");
-            
+    $(".categorie").each(function () {
+        $ciao = $(this).text().trim();
+        if ($ciao === $filter) {
+
+            $(this).css("background", "#286090");
+            $(this).css("color", "white");
+
         }
 
-    });      
-};
+    });
+}
+;
 
 function selectMash(nomeMU, idMU)
 {
@@ -32,9 +88,9 @@ function selectMash(nomeMU, idMU)
 
 function doHref(href)
 {
-    var nome= $("#nomeMash").val();
+    var nome = $("#nomeMash").val();
     var id = $("#idMash").val();
-    href = href +"&idMash="+id+"&nomeMash="+nome;
+    href = href + "&idMash=" + id + "&nomeMash=" + nome;
     var toRet = href.toString().replace(/\|\!/gi, "\'");
     console.log(toRet);
     window.location.replace(toRet);
